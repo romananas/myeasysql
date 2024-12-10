6b78e05bc2a858c5220423303a2b4da3ab8b30ad
romann <romann.belmer@gmail.com>
2024-12-10 17:33:31 +0100

    save

--- parse.go
+++ parse.go
@@ -6,12 +6,11 @@ import (
 )
 
 func _ParseQuerys(query string) []string {
-	re := regexp.MustCompile(`SELECT\s+(.*?)\s+FROM`)
-	match := re.FindStringSubmatch(query)
-	if len(match) == 0 {
+	step1 := regexp.MustCompile(`SELECT\s+(.*?)\s+FROM`).FindStringSubmatch(query)
+	if len(step1) == 0 {
 		return nil
 	}
-	splited := strings.Split(match[1], ",")
+	splited := strings.Split(step1[1], ",")
 	for i, s := range splited {
 		splited[i] = strings.TrimSpace(s)
 	}
