d5f719efc94c5d14abdbdf60a4a117ccba5d7e43
romann <romann.belmer@gmail.com>
2024-12-11 15:56:38 +0100

    cleaned comments

--- sort.go
+++ sort.go
@@ -4,21 +4,6 @@ import (
 	"strings"
 )
 
-// func _SortKeys(tags []string, names []string, keys []string) []int {
-// 	var order []int
-// 	for i := range names {
-// 		for j := range keys {
-// 			var cmpTag = len(tags) != 0 && strings.Compare(tags[i], keys[j]) == 0
-// 			var cmpName = strings.Compare(strings.ToLower(names[i]), strings.ToLower(keys[j])) == 0
-
-// 			if cmpTag || cmpName {
-// 				order = append(order, j)
-// 			}
-// 		}
-// 	}
-// 	return order
-// }
-
 func _SortKeys(tags []string, names []string, keys []string) []int {
 	var order []int
 	for _, key := range keys {
