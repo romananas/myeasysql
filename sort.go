6b9fbeed5702858b1aa81912b5f66e354ad5338f
romann <romann.belmer@gmail.com>
2024-12-11 15:31:48 +0100

    fixed logic

--- sort.go
+++ sort.go
@@ -4,24 +4,31 @@ import (
 	"strings"
 )
 
-type ptrToInt struct {
-	ptr any
-	i   int
-}
+// func _SortKeys(tags []string, names []string, keys []string) []int {
+// 	var order []int
+// 	for i := range names {
+// 		for j := range keys {
+// 			var cmpTag = len(tags) != 0 && strings.Compare(tags[i], keys[j]) == 0
+// 			var cmpName = strings.Compare(strings.ToLower(names[i]), strings.ToLower(keys[j])) == 0
 
-func _sortPtrs(a, b ptrToInt) bool {
-	return a.i < b.i
-}
+// 			if cmpTag || cmpName {
+// 				order = append(order, j)
+// 			}
+// 		}
+// 	}
+// 	return order
+// }
 
 func _SortKeys(tags []string, names []string, keys []string) []int {
 	var order []int
-	for i := range names {
-		for j := range keys {
-			var cmpTag = len(tags) != 0 && strings.Compare(tags[i], keys[j]) == 0
-			var cmpName = strings.Compare(strings.ToLower(names[i]), strings.ToLower(keys[j])) == 0
+	for _, key := range keys {
+		for i, name := range names {
+			var cmpTag = len(tags) != 0 && strings.Compare(tags[i], key) == 0
+			var cmpName = strings.Compare(strings.ToLower(name), strings.ToLower(key)) == 0
 
 			if cmpTag || cmpName {
-				order = append(order, j)
+				order = append(order, i)
+				break
 			}
 		}
 	}
